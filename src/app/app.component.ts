73c801e6d22802bbea8ec03eaacba191cced26f2
Peter Rifkind <prifkind@gmail.com>
2023-07-31 15:28:55 -0400

    Initial commit with scaffolding and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,66 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-training';
+  passwordLength = 0;
+  password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  onButtonClick() {
+    this.password = 'My Password';
+  }
+
+  onLengthChange(val: Event) {
+    const newLength = (val.target as HTMLInputElement).value;
+    if (newLength.length > 0 && !isNaN(+newLength)) {
+      this.passwordLength = +newLength;
+    }
+
+    console.log(`assigning length`, this.passwordLength)
+  }
+
+  useLettersChangeHandler() {
+    this.useLetters = !this.useLetters;
+  }
+
+  useNumbersChangeHandler() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  useSymbolsChangeHandler() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onGeneratePassword() {
+    console.log(`passwordLength`, this.passwordLength)
+    let characters = '';
+    let randomPassword = '';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*';
+
+    if (this.useLetters) {
+      characters += letters;
+    }
+
+    if (this.useNumbers) {
+      characters += numbers;
+    }
+
+    if (this.useSymbols) {
+      characters += symbols;
+    }
+
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * characters.length);
+
+      randomPassword += characters[index];
+    }
+
+    this.password = randomPassword;
+  }
 }
